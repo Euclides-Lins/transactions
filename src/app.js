fb06d9c8f0daf82cbcdc0201e1835b832fd636f0
Euclides-Lins <euclidesvasconcelos01@gmail.com>
2021-08-03 22:16:41 -0400

    test: ensure if users is listed, and check if have property name

--- src/app.js
+++ src/app.js
@@ -4,4 +4,11 @@ app.get('/', (req, res) => {
   res.send({ message: 'oi' });
 });
 
+app.get('/users', (req, res) => {
+  const users = [
+    { name: 'euclides', mail: 'euclides@a.com' },
+  ];
+  res.status(200).json(users);
+});
+
 module.exports = app;
